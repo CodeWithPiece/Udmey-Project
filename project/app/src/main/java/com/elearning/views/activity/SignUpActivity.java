8d14da5cc47166423f23d9e90d4b18bfef2d529d
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-14 11:41:12 +0530

    Adding activities and login ui done

---
+++ project/app/src/main/java/com/elearning/views/activity/SignUpActivity.java
@@ -0,0 +1,16 @@
+package com.elearning.views.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.elearning.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
