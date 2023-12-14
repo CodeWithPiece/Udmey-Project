43a88fe76f75fba1f7bfbaf8130925c9ea546cba
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-14 10:38:15 +0530

    Updating project structure

--- project/app/src/main/java/com/elearning/views/activity/MainActivity.java
+++ project/app/src/main/java/com/elearning/views/activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.elearning;
+package com.elearning.views.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.elearning.R;
 
 public class MainActivity extends AppCompatActivity {
 
