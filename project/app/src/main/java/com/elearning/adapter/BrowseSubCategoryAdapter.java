b75ed968d2e5587180aba0b96104f09264edb0c8
Nirmal <145927850+CodeWithPiece@users.noreply.github.com>
2023-12-16 09:26:15 +0530

    Working in browse category section

---
+++ project/app/src/main/java/com/elearning/adapter/BrowseSubCategoryAdapter.java
@@ -0,0 +1,47 @@
+package com.elearning.adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.elearning.R;
+import com.elearning.views.fragment.CourseFragment;
+
+public class BrowseSubCategoryAdapter extends RecyclerView.Adapter<BrowseSubCategoryAdapter.ViewHolder> {
+
+    CourseFragment courseFragment;
+
+    public BrowseSubCategoryAdapter(CourseFragment courseFragment) {
+        this.courseFragment = courseFragment;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_category_item_layout, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+
+        }
+    }
+
+}
